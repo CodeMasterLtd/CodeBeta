903201f047ed5e6d04c3b7949f13e229cc31bee0
CodeMasterLtd <info@codemaster.ltd>
2024-07-19 17:37:43 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -5,8 +5,25 @@ document.addEventListener("DOMContentLoaded", function() {
         'user2': 'password456'
     };
 
+    function addFontAndIconLinks() {
+        const head = document.head;
+
+        const iconLink = document.createElement('link');
+        iconLink.type = 'image/jpeg' && 'image/png';
+        iconLink.rel = 'apple-touch-icon' && 'icon';
+        iconLink.href = 'img/logo.png';
+        iconLink.size = '180x180' && '192x192' && '512x512';
+        head.appendChild(iconLink);
+    }
+
+    addFontAndIconLinks();
+
     const loginForm = document.getElementById('login-form');
     const infoDiv = document.getElementById('info');
+    const warningBox = document.getElementById('warning');
+    const welcome = document.getElementById('welcome');
+
+    warningBox.style.backgroundImage = "url('https://www.codemaster.ltd/cdn/shop/files/codemaster_1.jpg?v=1719057471&width=140')";
 
     loginForm.addEventListener('submit', function(event) {
         event.preventDefault();
@@ -17,12 +34,29 @@ document.addEventListener("DOMContentLoaded", function() {
         console.log(`Password: ${password}`);  // Debugging log
 
         if (validUsers[username] === password) {
+            welcome.innerHTML = 'Welcome, ' + (validUsers[username] ? '' + username : '');
+            warningBox.style.color = '#000';
+            warningBox.style.backgroundImage = "";
+            warningBox.style.backgroundColor = 'green';
+            warningBox.innerHTML = '<h3>Success</h3>';
+
+            infoDiv.style.fontSize = '1.2em';
+            infoDiv.style.color = 'green';
             infoDiv.innerHTML = '<p>Login successful! Redirecting...</p>';
             setTimeout(function() {
                 window.location.href = 'https://codemaster.ltd/pages/beta'; // Redirect to the dashboard or another page
-            }, 1000); // Delay for the message to be seen
+            }, 3000); // Delay for the message to be seen
         } else {
-            infoDiv.innerHTML = '<p>Invalid username or password.</p>';
+            welcome.innerHTML = '';
+            warningBox.style.color = '#000';
+            warningBox.style.backgroundImage = "";
+            warningBox.style.backgroundColor = 'red';
+            warningBox.innerHTML = '<h3>Error</h3>';
+            warningBox.style.boxShadow = '0 0 10px rgba(0, 0, 0, 0.5)';
+
+            infoDiv.style.fontSize = '1.2em';
+            infoDiv.style.color = 'red';
+            infoDiv.innerHTML = '<p>Invalid username or password, Please try again.</p>';
         }
     });
 });
