9f9203527f5d6f36c0d3db940a1e0b18c8c17b28
CodeMasterLtd <info@codemaster.ltd>
2024-07-22 00:48:31 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -1,30 +1,166 @@
-import { getUsers, getWebhook, updateUserPassword } from './users.js';
-const overrideTime = false;
+import { getUsers, getWebhook, updateUserPassword, bugError, getWebhookBug } from './users.js';
 
-document.addEventListener("DOMContentLoaded", function() {
-    const validUsers = getUsers();
-    const webhook = getWebhook();
+const loginForm = document.getElementById('login-form');
+const resetForm = document.getElementById('reset-form');
+const resetPasswordButton = document.getElementById('reset-password-button');
+const infoDiv = document.getElementById('info');
+const welcome = document.getElementById('welcome');
+const userrole = document.getElementById('user-role');
+const footer = document.getElementById('footer');
+const profilePicture = document.getElementById('profile-picture');
 
-    function addFontAndIconLinks() {
-        const head = document.head;
+let validUsers;
+const webhook = getWebhook();
+const bugerror = bugError();
+const bugwebhook = getWebhookBug();
+const BugNotify = true;
 
-        const iconLink = document.createElement('link');
-        iconLink.type = 'image/png';
-        iconLink.rel = 'icon' || 'shortcut icon' || 'apple-touch-icon';
-        iconLink.href = 'img/logo.png';
-        iconLink.sizes = '180x180' || '192x192' || '512x512';
-        head.appendChild(iconLink);
+function addFontAndIconLinks() {
+    const head = document.head;
+    const iconLink = document.createElement('link');
+    iconLink.type = 'image/png';
+    iconLink.rel = 'icon';
+    iconLink.href = 'img/logo.png';
+    iconLink.sizes = '180x180'; // Prefer single size; others can be added if needed
+    head.appendChild(iconLink);
+}
+
+function copyRight() {
+    const currentYear = new Date().getFullYear();
+    footer.innerHTML = (currentYear === 2024) ? "2024" : `2024 - ${currentYear}`;
+}
+
+function refreshUserData() {
+    validUsers = getUsers();
+}
+
+function timeOut() {
+    setTimeout(() => {
+        infoDiv.style.fontSize = '14px';
+        infoDiv.style.color = 'white';
+        infoDiv.innerHTML = `
+            <p>Welcome to Code Master Beta.</p>
+            <p>If you encounter any issues, please <a href="https://www.codemaster.ltd/pages/contact">contact us</a>.</p>
+        `;
+    }, 2000);
+}
+
+function form() {
+    setTimeout(() => {
+        if (loginForm) {
+            loginForm.style.display = 'block';
+            resetForm.style.display = 'none';
+        }
+    }, 2000);
+}
+
+function sendDiscordNotification1(discordID, action) {
+    const now = new Date();
+    const hours = now.getHours();
+    const minutes = now.getMinutes();
+    const day = now.getDay();
+    const allowedDays = [5, 6, 0]; // Friday, Saturday, Sunday
+    const startHour = 9;
+    const endHour = 22;
+
+    if (allowedDays.includes(day) || (hours >= startHour && hours < endHour)) {
+        const message = {
+            content: `User <@${discordID}> ${action}`,
+            username: 'Beta Login | Code Master'
+        };
+
+        fetch(webhook, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(message)
+        })
+        .then(response => {
+            if (response.ok) {
+                console.log('Discord notification sent successfully');
+            } else {
+                console.error('Failed to send Discord notification:', response.status, response.statusText);
+            }
+        })
+        .catch(error => console.error('Error sending Discord notification:', error));
+    } else {
+        console.log('Notification not sent: Outside allowed time window.');
+    }
+}
+
+function sendBugNotification(title, description, color, image) {
+    const embed = {
+        content: `<@&${bugerror}>`,
+        embeds: [
+            {
+                title: title,
+                description: description,
+                color: color, // Orange color in decimal (0xFFA500)
+                image: {
+                    url: image
+                },
+                footer: {
+                    text: "Code Master Beta"
+                }
+            }
+        ],
+        username: 'Beta Alerts | Code Master'
+    };
+
+    fetch(bugwebhook, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(embed)
+    })
+    .then(response => {
+        if (response.ok) {
+            console.log('Bug notification sent successfully');
+        } else {
+            console.error('Failed to send bug notification:', response.status, response.statusText);
+        }
+    })
+    .catch(error => console.error('Error sending bug notification:', error));
+}
+
+function time() {
+    const now = new Date();
+    const hours = now.getHours();
+    const minutes = now.getMinutes();
+    const seconds = now.getSeconds();
+
+    if (hours >= 3 && hours < 12) {
+        setTimeout(timeOut, 2500);
+        infoDiv.innerHTML = '<p>Good Morning</p>';
+    } else if (hours >= 12 && hours < 18) {
+        setTimeout(timeOut, 2500);
+        infoDiv.innerHTML = '<p>Good Afternoon</p>';
+    } else if (hours >= 18 && hours < 22) {
+        setTimeout(timeOut, 2500);
+        infoDiv.innerHTML = '<p>Good Evening</p>';
+    } else {
+        setTimeout(timeOut, 2500);
+        infoDiv.innerHTML = '<p>Good Night</p>';
     }
 
-    addFontAndIconLinks();
+    if (hours === 0 && minutes === 0 && seconds === 0) {
+        if (BugNotify === true) {
+            sendBugNotification("Services Unavailable",  "We are currently experiencing issues with our services. Please try again later.", 16753920, "https://github.com/CodeMasterLtd/CodeBeta/blob/main/img/down.png?raw=true");
+        } else if (BugNotify === false) {
+            sendBugNotification("Services Online",  "Everything seems normal here.", 65280, "https://github.com/CodeMasterLtd/CodeBeta/blob/main/img/online.png?raw=true");
+        }
+    }
+}
 
-    const loginForm = document.getElementById('login-form');
-    const resetForm = document.getElementById('reset-form');
-    const resetPasswordButton = document.getElementById('reset-password-button');
-    const infoDiv = document.getElementById('info');
-    const welcome = document.getElementById('welcome');
-    const userrole = document.getElementById('user-role');
-    const footer = document.getElementById('footer');
+document.addEventListener("DOMContentLoaded", function() {
+    resetPasswordButton.style.display = 'none';
+    validUsers = getUsers();
+    
+    addFontAndIconLinks();
+    copyRight();
+    time(); // Call time function here
 
     if (loginForm) {
         loginForm.addEventListener('submit', function(event) {
@@ -51,11 +187,16 @@ document.addEventListener("DOMContentLoaded", function() {
                 infoDiv.innerHTML = '<p>Login successful! Redirecting...</p>';
                 sendDiscordNotification1(user.discordID, 'has successfully logged in.');
                 userrole.innerHTML = '<h3>User Role: ' + user.role + '</h3>';
-                setTimeout(function() {
+
+                if (user.discordPhoto) {
+                    profilePicture.src = user.discordPhoto;
+                    profilePicture.style.boxShadow = 'orange 0 0 10px';
+                }
+                
+                setTimeout(() => {
                     window.location.href = './beta.html'; // Redirect to the dashboard or another page
-                }, 3000); // Delay for the message to be seen
+                }, 3000);
             } else {
-                // Check for specific errors
                 const validUser = validUsers.find(user => 
                     user.name === username || 
                     user.password === password || 
@@ -63,37 +204,32 @@ document.addEventListener("DOMContentLoaded", function() {
                 );
     
                 if (!validUser) {
-                    // All credentials are wrong
                     infoDiv.style.fontSize = '1.2em';
                     infoDiv.style.color = 'red';
                     infoDiv.innerHTML = '<p>Invalid username, password, or role. Please try again.</p>';
-                    timeOut();
                 } else {
-                    // Specific errors
                     if (validUser.name !== username) {
                         infoDiv.style.fontSize = '1.2em';
                         infoDiv.style.color = 'red';
                         infoDiv.innerHTML = '<p>Invalid username.</p>';
-                        timeOut();
                     } else if (validUser.password !== password) {
                         infoDiv.style.fontSize = '1.2em';
                         infoDiv.style.color = 'red';
                         infoDiv.innerHTML = '<p>Invalid password.</p>';
-                        timeOut();
                     } else if (validUser.role.toLowerCase() !== selectedRole.toLowerCase()) {
                         infoDiv.style.fontSize = '1.2em';
                         infoDiv.style.color = 'red';
                         infoDiv.innerHTML = '<p>Invalid role.</p>';
-                        timeOut();
-                    } else if (validUser.name !== username && validUser.password !== password && validUser.role.toLowerCase() !== selectedRole.toLowerCase()) {
+                    } else {
                         infoDiv.style.fontSize = '1.2em';
                         infoDiv.style.color = 'amber';
                         infoDiv.innerHTML = `<p>You don't have an account!.</p>`;
-                        timeOut();
                     }
                 }
     
                 welcome.innerHTML = '';
+                profilePicture.src = 'https://www.codemaster.ltd/cdn/shop/files/codemaster_1.jpg?v=1719057471';
+                profilePicture.style.boxShadow = 'orange 0 0 10px';
             }
         });
     }    
@@ -112,10 +248,15 @@ document.addEventListener("DOMContentLoaded", function() {
             const resetUsername = document.getElementById('reset-username').value.trim();
             const newPassword = document.getElementById('new-password').value.trim();
             
-            let user = validUsers.find(user => user.name === resetUsername);
+            // Update the password
+            updateUserPassword(resetUsername, newPassword);
             
+            // Refresh the user data
+            refreshUserData();
+            
+            // Check if update was successful
+            const user = validUsers.find(user => user.name === resetUsername);
             if (user) {
-                updateUserPassword(resetUsername, newPassword);
                 infoDiv.style.fontSize = '1.2em';
                 infoDiv.style.color = 'green';
                 infoDiv.innerHTML = '<p>Password reset successful! You can now log in with your new password.</p>';
@@ -131,76 +272,4 @@ document.addEventListener("DOMContentLoaded", function() {
             }
         });
     }
-
-    function timeOut() {
-        setTimeout(function() {
-            infoDiv.style.fontSize = '14px';
-            infoDiv.style.color = 'white';
-            infoDiv.innerHTML = `
-                <p>Welcome to Code Master Beta.</p>
-                <p>If you encounter any issues, please <a href="https://www.codemaster.ltd/pages/contact">contact us</a>.</p>
-            `;
-        }, 2000);
-    }
-
-    function form() {
-        setTimeout(function() {
-            if (loginForm) {
-                loginForm.style.display = 'block';
-                resetForm.style.display = 'none';
-            }
-        }, 2000)
-    }
-
-
-    function copyRight() {
-        if (footer) {
-            const currentYear = new Date().getFullYear();
-            if (currentYear === 2024) {
-                footer.innerHTML = " 2024";
-            } else {
-                footer.innerHTML = ` 2024 - ${currentYear}`;
-            }
-        }
-    }
-
-    function sendDiscordNotification1(discordID, action) {
-        const now = new Date();
-        const hours = now.getHours();
-        const day = now.getDay();
-    
-        const allowedDays = [5, 6, 0];
-
-        // Define the time window for sending notifications (9 AM to 10 PM)
-        const startHour = 9;
-        const endHour = 22;
-    
-        // Check if the current time is within the allowed time window
-        if (allowedDays.includes(day) || (overrideTime === true || (hours >= startHour && hours < endHour))) {
-            const message = {
-                content: `User <@${discordID}> ${action}`,
-                username: 'Beta Login | Code Master'
-            };
-    
-            fetch(webhook, {
-                method: 'POST',
-                headers: {
-                    'Content-Type': 'application/json',
-                },
-                body: JSON.stringify(message)
-            })
-            .then(response => {
-                if (response.ok) {
-                    console.log('Discord notification sent successfully');
-                } else {
-                    console.error('Failed to send Discord notification:', response.status, response.statusText);
-                }
-            })
-            .catch(error => console.error('Error sending Discord notification:', error));
-        } else {
-            console.log('Notification not sent: Outside allowed time window.');
-        }
-    }    
-
-    copyRight();
 });
--- users.js
+++ users.js
@@ -1,30 +1,41 @@
 export const webhook = 'https://discord.com/api/webhooks/1264315006854496288/JZz_aHj85UAm5aipAPkbV2eWtNj4IddhhwQ1SKNF5AcIPbpStWTa4BH1SSG_eUFmyZCD';
-export const downloadHook = 'https://discord.com/api/webhooks/1264374034481807380/LPro6kkYJdvwM_rBTjhNRYqrvhKDLxFgw95zuhcdP2CzX8tUtmKXYhCIUmhO7BDcJa26';
+export const bug = '1236105830051483759';
+export const webhookBug = 'https://discord.com/api/webhooks/1264374034481807380/LPro6kkYJdvwM_rBTjhNRYqrvhKDLxFgw95zuhcdP2CzX8tUtmKXYhCIUmhO7BDcJa26';
 
 export const admins = [
     {
-        "discordID": "1030462659092545546",
+        "discordID": "1030462659092545546", // CEO
         "name": "Kieran",
         "password": "Caitlin230124",
-        "role": "Admin"
+        "role": "Admin",
+        "discordPhoto": 'https://cdn.discordapp.com/avatars/1030462659092545546/600bd29a4928ee579815a44e2fd17515.png?size=1024'
+    },
+    {
+        "discordID": "794738803600261120", // COO
+        "name": "Ellwood",
+        "password": "Ellwood21072024",
+        "role": "Admin",
+        "discordPhoto": 'https://cdn.discordapp.com/avatars/794738803600261120/2db54fe9dc04bbc46f83bd534e78bc7c.png?size=1024'
     }
 ];
 
 export const staff = [
     {
-        "discordID": "871902966540542002",
+        "discordID": "871902966540542002", // Staff
         "name": "Skutela",
         "password": "Skutela21072024",
-        "role": "Staff"
+        "role": "Staff",
+        "discordPhoto": 'https://cdn.discordapp.com/avatars/871902966540542002/04ef2b881c3d337cb47dc5c695ffe4e3.png?size=1024'
     }
 ];
 
 export const beta = [
     {
-        "discordID": "861592808179499008",
+        "discordID": "861592808179499008", /// Beta
         "name": "ByQuadiix",
         "password": "ByQuadiix21072024",
-        "role": "Beta"
+        "role": "Beta",
+        "discordPhoto": 'https://cdn.discordapp.com/avatars/861592808179499008/80976390e71deed834c972cbde025087.png?size=1024'
     }
 ];
 
@@ -36,8 +47,12 @@ export function getWebhook() {
     return webhook;
 }
 
-export function getDownloadWebhook() {
-    return downloadHook;
+export function getWebhookBug() {
+    return webhookBug;
+}
+
+export function bugError() {
+    return bug;
 }
 
 export function updateUserPassword(username, newPassword) {
